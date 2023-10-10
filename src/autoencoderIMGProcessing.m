15d09018005e430e52ef9010d3e4fc854c45ed89
akappakappa <andreavalde36@gmail.com>
2023-10-10 17:14:22 +0200

    structure

---
+++ src/autoencoderIMGProcessing.m
@@ -0,0 +1,21 @@
+% Alessio Cocco 2087635 Andrea Valentinuzzi 2090451, Giovanni Brejc 2096046
+% Convolutional Autoencoder for image colorization on 16 planktic foraminifera images
+% Matlab R2023b
+
+
+% INITIALIZATION - Folders & paths
+
+
+% INPUT - 16 images of planktic foraminifera
+
+
+% PREPROCESSING - Image resizing to 16x224x224
+
+
+% TRAINING - Convolutional Autoencoder
+
+
+% EXTRACTION - 3x224x224 image
+
+
+% OUTPUT - colorized image
