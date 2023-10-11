852929122098019aa06724b5876299aea150e662
akappakappa <andreavalde36@gmail.com>
2023-10-11 15:44:10 +0200

    input

--- src/autoencoderIMGProcessing.m
+++ src/autoencoderIMGProcessing.m
@@ -7,6 +7,7 @@ clear all
 clc
 baseF = 'assignment/dataset';   % dataset folder
 outF = 'AUTOENCIMG';            % colorized images folder
+imgS = [224 224];               % image size
 path = dir(baseF);
 path = {path([path.isdir] & ~ismember({path.name}, {'.', '..'})).name};
 if not(isfolder(outF))
@@ -14,28 +15,33 @@ if not(isfolder(outF))
 end
 
 % INPUT - 16 images of planktic foraminifera
-DATA = path;                                    % initialize dataset
-for K = 1 : length(path)                        % for each folder
-    imds = imageDatastore(...                   % image datastore
+disp('> INPUT:');
+DATA = path;                                      % initialize dataset
+for K = 1 : length(path)                          % for each folder
+    disp(path{K});
+    imds = imageDatastore(...                     % image datastore
         fullfile(baseF, path{K}), ...
         'IncludeSubfolders', true, ...
         'LabelSource', 'foldernames');
     
-    if not(isfolder(path{K}))
-        mkdir(outF, path{K});                   % create folder for colorized images
+    if not(isfolder(fullfile(outF, path{K})))
+        mkdir(outF, path{K});                     % create folder for colorized images
     end
-    
-    I = 1;
-    while I < length(imB.Labels)                % for each image
-        [imgR, imgC] = size(readimage(imds, I));
-        px = zeros(imgR, imgC, 16);             % initialize colorized image
 
-        for J = 1 : 16                          % for each channel (batches of 16)
-            px(:, :, J) = readimage(imds, I);   % copy grayscale image
-            I = I + 1;                          % next image
+    I = 1;
+    while I < length(imds.Labels)                 % for each image
+        px = zeros(imgS(1), imgS(2), 16);         % initialize colorized image
+
+        for J = 1 : 16                            % for each channel (batches of 16)
+            imgT = readimage(imds, I);            % read image
+            if size(imgT, 3) ~= 1
+                imgT = rgb2gray(imgT);            % convert to grayscale
+            end
+            px(:, :, J) = imresize(imgT, imgS);   % resize image
+            I = I + 1;                            % next image
         end
 
-
+        DATA{2, K}{(I - 1) / 16} = px;            % save colorized image
     end
 end
 
